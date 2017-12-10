a5b6f5360bcf6351765d6501d698b60cd99574d1
Nick <nicksalt24@gmail.com>
2017-12-10 03:35:49 -0500

    Updated AboutViewController

--- WalkTest/AboutViewController.swift
+++ WalkTest/AboutViewController.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2017 OHRI. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let url = Bundle.main.url(forResource: "HTML/aboutPage", withExtension: "html")
+        let request = URLRequest(url: url!)
+        webView.loadRequest(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
